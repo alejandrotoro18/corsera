6e92a245bc82a5b13dbd65904738576cd70b5155
alejandrotoro18 <alejandrotoro18@gmail.com>
2022-06-17 21:14:02 -0300

    1 file modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { SharedModule } from './shared/shared.module';
+import { GifsModule } from './gifs/gifs.module';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule,SharedModule],
+  imports:      [ BrowserModule, FormsModule,SharedModule,GifsModule],
   declarations: [ AppComponent, HelloComponent ],
   bootstrap:    [ AppComponent ]
 })
---
+++ src/app/gifs/gifs.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { GifspageComponent } from './gifspage/gifspage.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [GifspageComponent]
+  export: [GifspageComponent]
+})
+export class GifsModule { }
